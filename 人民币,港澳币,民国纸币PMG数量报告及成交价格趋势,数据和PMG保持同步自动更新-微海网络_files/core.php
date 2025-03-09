469262ebb414b44d0860b87236b13d75c604338d
jimmy0200 <mcdull02@foxmail.com>
2025-03-09 18:46:51 +0800

    Add files via upload

---
+++ 人民币,港澳币,民国纸币PMG数量报告及成交价格趋势,数据和PMG保持同步自动更新-微海网络_files/core.php
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+!function(){var p,q,r,a=encodeURIComponent,b="1261740908",c="",d="",e="online_v3.php",f="z11.cnzz.com",g="1",h="text",i="z",j="&#31449;&#38271;&#32479;&#35745;",k=window["_CNZZDbridge_"+b]["bobject"],l=function(){var scripts=document.getElementsByTagName('script');for(var i=0,len=scripts.length;i<len;i++){var a,el=scripts[i];if(el.src&&(a=(/^(https?:)\/\/[\w\.\-]+\.cnzz\.com\//i).exec(el.src))){return a[1];}}return location.protocol;}(),m="1",n=l+"//web.archive.org/web/20180822125925/https://online.cnzz.com/online/"+e,o=[];o.push("id="+b),o.push("h="+f),o.push("on="+a(d)),o.push("s="+a(c)),n+="?"+o.join("&"),"0"===m&&k["callRequest"]([l+"//web.archive.org/web/20180822125925/https://cnzz.mmstat.com/9.gif?abc=1"]),g&&(""!==d?k["createScriptIcon"](n,"utf-8"):(q="z"==i?"https://web.archive.org/web/20180822125925/http://www.cnzz.com/stat/website.php?web_id="+b:"https://web.archive.org/web/20180822125925/http://quanjing.cnzz.com","pic"===h?(r=l+"//web.archive.org/web/20180822125925/https://icon.cnzz.com/img/"+c+".gif",p="<a href='"+q+"' target=_blank title='"+j+"'><img border=0 hspace=0 vspace=0 src='"+r+"'></a>"):p="<a href='"+q+"' target=_blank title='"+j+"'>"+j+"</a>",k["createIcon"]([p])))}();
+
+}
+/*
+     FILE ARCHIVED ON 12:59:25 Aug 22, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 09:19:48 Mar 09, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.684
+  exclusion.robots: 0.024
+  exclusion.robots.policy: 0.01
+  esindex: 0.013
+  cdx.remote: 21.019
+  LoadShardBlock: 117.059 (3)
+  PetaboxLoader3.datanode: 101.253 (4)
+  PetaboxLoader3.resolve: 128.158 (2)
+  load_resource: 132.462
+*/
